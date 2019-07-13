a10cb3fb0ac649eda1c81a81ee95cc4ffb03ae82
baraabourghli <baraa.bourghli@gmail.com>
2019-07-13 12:37:25 +0800

    initial commit

---
+++ src/index.js
@@ -0,0 +1,9 @@
+import myCurrentLocation, { message, name, getGreeting } from './myModule'
+import add, { sub } from './math'
+
+console.log(message)
+console.log(name)
+console.log(myCurrentLocation)
+console.log(getGreeting('Baraa'))
+console.log(add(1, 1))
+console.log(sub(1, 1))
