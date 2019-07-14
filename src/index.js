f98894715fe58c025493d38ccfbbe112a1fb3385
baraabourghli <baraa.bourghli@gmail.com>
2019-07-14 23:00:33 +0800

    Mutations basics

--- src/index.js
+++ src/index.js
@@ -1,4 +1,5 @@
 import { GraphQLServer } from 'graphql-yoga'
+import uuidv4 from 'uuid/v4'
 
 // Demo Data
 const users = [{
@@ -69,6 +70,10 @@ const typeDefs = `
         post: Post!
     }
 
+    type Mutation {
+        createUser(name: String!, email: String!, age: Int): User!
+    }
+
     type User {
         id: ID!
         name: String!
@@ -135,6 +140,26 @@ const resolvers = {
         },
         comments(parent, args, ctx, info) {
             return comments
+        }
+    },
+    Mutation: {
+        createUser(parent, args, ctx, info) {
+            const emailTaken = users.some((user) => user.email === args.email)
+            
+            if(emailTaken) {
+                throw new Error('Email taken.')
+            }
+
+            const user = {
+                id: uuidv4(),
+                name: args.name,
+                email: args.email,
+                age: args.age
+            }
+
+            users.push(user)
+
+            return user
         }
     },
     Post: {
