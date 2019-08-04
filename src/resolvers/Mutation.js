8e260919e1ce138c08653921fd8b87fceefb4c56
baraabourghli <baraa.bourghli@gmail.com>
2019-08-04 20:11:20 +0800

    more update mutations

--- src/resolvers/Mutation.js
+++ src/resolvers/Mutation.js
@@ -1,4 +1,5 @@
 import uuidv4 from 'uuid/v4'
+import { type } from 'os';
 
 const Mutation = {
     createUser(parent, args, { db }, info) {
@@ -96,6 +97,38 @@ const Mutation = {
 
         return deletedPosts[0]
     },
+    updatePost(parent, args, { db }, info) {
+        const { id, data } = args
+        const post = db.posts.find((post) => post.id === id)
+
+        if (!post) {
+            throw new Error('Post not found')
+        }
+
+        if (typeof data.title === 'string') {
+            post.title = data.title
+        }
+
+        if (typeof data.body === 'string') {
+            post.body = data.body
+        }
+
+        if (typeof data.published === 'boolean') {
+            post.published = data.published
+        }
+
+        if (typeof data.author === 'string') {
+            const authorExist = db.users.find((user) => user.id === data.author)
+
+            if(!authorExist) {
+                throw new Error('Author not found')
+            }
+
+            post.author = data.author
+        }
+
+        return post
+    },
     createComment(parent, args, { db }, info) {
         const userExists = db.users.some((user) => user.id === args.data.author)
         const postExists = db.posts.some((post) => post.id === args.data.post && post.published)
@@ -127,6 +160,20 @@ const Mutation = {
         const deletedComments = db.comments.splice(commentIndex, 1)
 
         return deletedComments[0]
+    },
+    updateComment(parent, args, { db }, info) {
+        const {id, data} = args
+        const comment = db.comments.find((comment) => comment.id === id)
+
+        if(!comment) {
+            throw new Error('Comment not found')
+        }
+
+        if (typeof data.text === 'string') {
+            comment.text = data.text
+        }
+
+        return comment
     }
 }
 
